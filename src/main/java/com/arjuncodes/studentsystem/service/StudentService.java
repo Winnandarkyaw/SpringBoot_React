a7457b4a656c2d89f63cecc249ffa1dc6a082798
Winnandarkyaw <winnandarkyaw2000@gmail.com>
2024-09-05 15:14:47 +0630

    initial

---
+++ src/main/java/com/arjuncodes/studentsystem/service/StudentService.java
@@ -0,0 +1,8 @@
+package com.arjuncodes.studentsystem.service;
+
+import com.arjuncodes.studentsystem.model.Student;
+import org.springframework.stereotype.Service;
+
+public interface StudentService {
+    public Student saveStudent(Student student);
+}
