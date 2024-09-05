fc8b2050274837d949f409b00e35e3d4d2c06640
Winnandarkyaw <winnandarkyaw2000@gmail.com>
2024-09-05 15:32:23 +0630

    getDatabase

--- src/main/java/com/arjuncodes/studentsystem/service/StudentService.java
+++ src/main/java/com/arjuncodes/studentsystem/service/StudentService.java
@@ -3,6 +3,9 @@ package com.arjuncodes.studentsystem.service;
 import com.arjuncodes.studentsystem.model.Student;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
