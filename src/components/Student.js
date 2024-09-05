2cd539aabcf0e8194cec7d92304a3bff16497184
Winnandarkyaw <winnandarkyaw2000@gmail.com>
2024-09-05 20:35:59 +0630

    adding

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,5 +1,7 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { Container, Paper } from "@mui/material";
 export default function Student() {
@@ -8,10 +10,35 @@ export default function Student() {
     width: 600,
     margin: "200px auto",
   };
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [students, setStudents] = useState([]);
+  const handleClick = (e) => {
+    e.preventDefault();
+    const student = { name, address };
+    console.log(student);
+    fetch("http://localhost:8080/student/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    }).then(() => {
+      console.log("New Student added");
+    });
+  };
+  useEffect(() => {
+    fetch("http://localhost:8080/student/getAll")
+      .then((res) => res.json())
+      .then((result) => {
+        setStudents(result);
+      });
+  }, []);
+
   return (
     <Container>
       <Paper elevation={3} style={paperStyle}>
-        <h1>Add Student</h1>
+        <h1 style={{ color: "blue" }}>
+          <u>Add Student</u>
+        </h1>
         <Box
           component="form"
           sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
@@ -21,13 +48,37 @@ export default function Student() {
             id="outlined-basic"
             label="Student Name"
             variant="outlined"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <TextField
             id="outlined-basic"
             label="Student Address"
             variant="outlined"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
           />
+          <Button variant="outlined" onClick={handleClick}>
+            Submit
+          </Button>
         </Box>
+        {name}
+        {address}
+      </Paper>
+      <h1>Students</h1>
+      <Paper elevation={3} style={paperStyle}>
+        {students.map((student) => (
+          <Paper
+            elevation={6}
+            style={{ margin: "10px", padding: "15px", textAlign: "left" }}
+            key={student.id}>
+            Id:{student.id}
+            <br />
+            Name:{student.name}
+            <br />
+            Address:{student.address}
+          </Paper>
+        ))}
       </Paper>
     </Container>
   );
